d948a9135222b77927b4b6b0aad7b93a2e3cc160
George Thomas <george.thomas@quintype.com>
2019-07-24 10:00:09 +0530

    Add valid status codes for failure and remove 2xx status from failure

--- server.rb
+++ server.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 
+FAILURE_STATUS = [300, 301, 302, 303, 304, 305, 306, 307, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 500, 501, 502, 503, 504, 505].freeze
+
 probabilistic_response = lambda do
   probability = params[:probability]&.to_i || 100
 
@@ -7,7 +9,7 @@ probabilistic_response = lambda do
     status 200
     body { { success: true }.to_json }
   else
-    http_status = (201..599).to_a.sample
+    http_status = FAILURE_STATUS.sample
     status http_status
   end
 end
